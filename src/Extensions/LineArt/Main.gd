c037f92458447e7e4fc463750b91e67c416b9d03
Variable <pkfayezakhtar@gmail.com>
2024-08-22 21:04:37 +0500

    version 0.3, should now work for pixelorama 1.0.2

--- src/Extensions/LineArt/Main.gd
+++ src/Extensions/LineArt/Main.gd
@@ -2,19 +2,22 @@ extends Node
 
 var item_id: int
 var lineart_dialog: ImageEffect
+var api: Node
 
 # This script acts as a setup for the extension
 func _enter_tree() -> void:
-	var type = ExtensionsApi.menu.EFFECTS
+	api = get_node_or_null("/root/ExtensionsApi")
+	if api:
+		var type = api.menu.EFFECTS
 
-	item_id = ExtensionsApi.menu.add_menu_item(type, "Make LineArt", self)
-	# the 3rd argument (in this case "self") will try to call "menu_item_clicked"
-	# (if it is present)
+		item_id = api.menu.add_menu_item(type, "Make LineArt", self)
+		# the 3rd argument (in this case "self") will try to call "menu_item_clicked"
+		# (if it is present)
 
-	lineart_dialog = preload(
-		"res://src/Extensions/LineArt/Assets/Dialog/LineArtDialog.tscn"
-	).instantiate()
-	ExtensionsApi.dialog.get_dialogs_parent_node().add_child(lineart_dialog)
+		lineart_dialog = preload(
+			"res://src/Extensions/LineArt/Assets/Dialog/LineArtDialog.tscn"
+		).instantiate()
+		api.dialog.get_dialogs_parent_node().add_child(lineart_dialog)
 
 
 
@@ -24,6 +27,7 @@ func menu_item_clicked():
 
 
 func _exit_tree() -> void:  # Extension is being uninstalled or disabled
-	# remember to remove things that you added using this extension
-	ExtensionsApi.menu.remove_menu_item(ExtensionsApi.menu.EFFECTS, item_id)
-	lineart_dialog.queue_free()
+	if api:
+		# remember to remove things that you added using this extension
+		api.menu.remove_menu_item(ExtensionsApi.menu.EFFECTS, item_id)
+		lineart_dialog.queue_free()
